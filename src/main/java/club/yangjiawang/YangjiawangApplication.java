8eaa11b78fbb2db1db283b9480c33ebc9f514dc7
yangjiawang <yangjiawang@qq,com>
2019-12-05 13:31:23 +0800

    个人网站第一次提交

---
+++ src/main/java/club/yangjiawang/YangjiawangApplication.java
@@ -0,0 +1,13 @@
+package club.yangjiawang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangjiawangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YangjiawangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/club/yangjiawang/YangjiawangApplicationTests.java
@@ -0,0 +1,13 @@
+package club.yangjiawang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YangjiawangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
